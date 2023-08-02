37f4a897b4cfd31e42d03ab86a938adb7cab3bdf
Larry <peggydbc1217@gmail.com>
2023-08-02 14:04:20 +0800

    homepage

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：ss
   // base: '/Repository 的名稱/'
-  base: "/web-layout-training-vite/",
+  base: "/GlassWebsite-RWD-Pratice/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
